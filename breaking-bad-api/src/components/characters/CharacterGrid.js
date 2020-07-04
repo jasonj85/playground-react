2df3cb7f1a92cd2dbbf8fa3e452e5a09dd06d9c3
jasonj85 <jasonj85@gmail.com>
2020-07-04 12:23:34 +0100

    Final changes to breaking bad app, which uses React & Axios

--- breaking-bad-api/src/components/characters/CharacterGrid.js
+++ breaking-bad-api/src/components/characters/CharacterGrid.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
+import CharacterItem from './CharacterItem';
+import Spinner from '../ui/Spinner';
 
 const CharacterGrid = ({ items, isLoading }) => {
     return (
         isLoading ?
             (
-                <h2>Loading...</h2>
+                <Spinner />
             )
             :
             (
                 <section className="cards">
                     {items.map(item => (
-                        <h1>{item.name}</h1>
+                        <CharacterItem key={item.char_id} item={item} />
                     ))}
                 </section>
             )
