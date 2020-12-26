541c2266a1a7cc730fe06584ae5fd7593a1741ca
jasonj85 <jasonj85@gmail.com>
2020-12-26 08:02:19 +0000

    Testing out some webpack configuration options

--- webpack-demo/src/components/PizzaImage/PizzaImage.js
+++ webpack-demo/src/components/PizzaImage/PizzaImage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './PizzaImage.css';
+import PizzaImage from '../../assets/pizza.jpg';
 
 const pizzaImage = (props) => (
   <div className={classes.PizzaImage}>
